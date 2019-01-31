6e2ce48279b038d16bb49552cc6a1f2796727fff
Rithwik Ediga Lakhamsani <ricky.ediga@gmail.com>
2019-01-31 00:13:40 -0800

    adjusted model with color maps

--- conv_model.py
+++ conv_model.py
@@ -8,49 +8,43 @@ import os
 import glob
 import matplotlib.pyplot as plt
 
-
-def predict(val, blackAndWhite):
+# predicting a value
+def predict(val, image_type = "jet"):
 	first_img = orig_stereos[val]
-	test_plot_image(first_img, blackAndWhite)
+	plot_image(first_img)
 	output = stereo_model.predict(np.array([stereos[val]]), batch_size = 1)
-	test_plot_image(orig_depths[val], blackAndWhite)
-	test_plot_image(np.reshape(output[0], [100,150]), blackAndWhite)
+	plot_image(orig_depths[val], image_type)
+	plot_image(np.reshape(output[0], [100,150]), image_type)
 
 # This is to load an image from pillow and convert it into a numpy array
-def load_image(infilename, blackAndWhite):
+def load_image(infilename):
 	img = Image.open(infilename)
-	if blackAndWhite:
-		img = img.convert("L")
+	img = img.convert("L")
 	img.load()
-	data = np.asarray( img, dtype="int32" )
+	data = np.asarray(img, dtype="int32")
 	return data
 
 # This is to display the image
-def test_plot_image(input, blackAndWhite):
-	if blackAndWhite:
-		plt.imshow(input, cmap = "Greys")
+def plot_image(input, type_of_cmap = "Greys"):
+	if type_of_cmap:
+		plt.imshow(input, cmap = type_of_cmap)
 	else:
 		plt.imshow(input)
 	plt.show()
 
 # hyperparameters
-shape_of_input_color = (100, 150, 8)
-shape_of_output_color = (100, 150, 4)
 shape_of_input = (100, 150, 2)
 shaoe_of_output = (100, 150, 1)
 
 # This is to import and preprocess the data
-def import_data(blackAndWhite):
-	stereos = sorted(glob.glob("./StereoImages/*.png"))
-	depths = sorted(glob.glob("./Depth_map/*.png"))
-	stereos = np.array([load_image(stereo, blackAndWhite) for stereo in stereos])
-	depths = np.array([load_image(depth, blackAndWhite) for depth in depths])
-	if blackAndWhite:
-		stereos_after = np.array([np.reshape(i, [100,300,1]) for i in stereos])
-		depths_after = np.array([np.reshape(i, [100,150,1]) for i in depths])
-	else:
-		stereos_after = stereos
-		depths_after = depths
+def import_data():
+	stereos = sorted(glob.glob("./stereo_images/*.png"))
+	depths = sorted(glob.glob("./depth_images/*.png"))
+	stereos = np.array([load_image(stereo) for stereo in stereos])
+	depths = np.array([load_image(depth) for depth in depths])
+	stereos_after = np.array([np.reshape(i, [100,300,1]) for i in stereos])
+	depths_after = np.array([np.reshape(i, [100,150,1]) for i in depths])
+
 
 	new_stereos = []
 	for i in range(stereos_after.shape[0]):
@@ -64,7 +58,7 @@ def import_data(blackAndWhite):
 
 	steroes_offset = np.mean(new_stereos, 0)
 	steroes_scale = np.std(new_stereos, 0).clip(min=1)
-	depths_scale = depths.max()
+	depths_scale = np.max(depths)
 
 	steroes_old_offset = np.mean(stereos, 0)
 	steroes_old_scale = np.std(stereos, 0).clip(min = 1)
@@ -76,32 +70,28 @@ def import_data(blackAndWhite):
 
 	return firstOutput, secondOutput, thirdOutput, lastOutput 
 
-# This is the model creation
-def createModel(blackAndWhite):
-	def createConvLayer(num_filters, input_tensor):
+# This is the first model
+def model_one():
+	def createConvLayer(num_filters, input_tensor, activation_type = "relu"):
 		conv = SeparableConv2D(num_filters, (3,3), data_format = "channels_last", padding = "same")(input_tensor)
 		conv = BatchNormalization()(conv)
-		conv = Activation("relu")(conv)
+		conv = Activation(activation_type)(conv)
 		return conv
 
-	if blackAndWhite:
-		visible = Input(shape_of_input)
-	else:
-		visible = Input(shape_of_input_color)
+	visible = Input(shape_of_input)
 	conv1 = createConvLayer(32, visible)
 	maxpool1 = MaxPooling2D((2,2))(conv1)
 	conv2 = createConvLayer(64, maxpool1)
 	conv3 = createConvLayer(64, conv2)
 	upscale = UpSampling2D((2,2))(conv3)
 	conv4 = createConvLayer(32, upscale)
-	if blackAndWhite:
-		output = createConvLayer(1, conv4)
-	else:
-		output = createConvLayer(4, conv4)
+	output = createConvLayer(1, conv4, "sigmoid")
+
 	model = Model(inputs = visible, outputs = output)
 	return model
 
-def createNewModel():
+# This is the second model
+def model_two():
 	def ConvLayer(num_filters, filter_size, input):
 		conv = SeparableConv2D(num_filters, filter_size, data_format = "channels_last", padding = "same")(input)
 		conv = BatchNormalization()(conv)
@@ -124,13 +114,15 @@ def createNewModel():
 	model = Model(inputs = visible, outputs = deconv4)
 	return model
 
-stereos, depths, orig_stereos, orig_depths = import_data(True)
+stereos, depths, orig_stereos, orig_depths = import_data()
 
-stereo_model = createModel(True)
+stereo_model = model_one()
 print(stereo_model.summary())
 stereo_model.compile(optimizer = Adam(0.02, 0.005), loss = "binary_crossentropy")
 
 stereo_model.fit(x = stereos, y = depths, batch_size = 32, epochs = 15, validation_split = 0.1)
 
-for i in range(2):
-	predict(100 + i, True)
+for i in range(3):
+	predict(100 + i)
+
+stereo_model.save_weights('adjusted_model.h5')
